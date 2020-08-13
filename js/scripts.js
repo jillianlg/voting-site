f597a1a98f8af8423ae8e8d5da1f9ffb0b51d987
Jillian Gibson <jillianlgibson@gmail.com>
2020-08-13 10:30:08 -0700

    first commit - add informational groups to html page, crete jquery logic to show groups based on user input, addes tyling
    CO AUTHORED by: Jillian Gibson <jillian.l.gibson@gmail.com

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// The page should prompt the user to enter their current age. -->
+// If the user is too young to vote, they should see different information than if the user is old enough to legally vote. -->
+// Minors should be offered links to websites where they may learn about the voting process and age requirements. -->
+// Those old enough to vote should be offered links to websites to register to vote, locate their nearest polling location/ballot dropbox is, or list when upcoming elections will take place. -->
+
+$(document).ready(function() {
+  const age = parseInt(prompt("How old are you?"));
+
+  if (age >= 18) {
+    $("#vote").show();
+  } else {
+    $("#under-18").show();
+  }
+});
